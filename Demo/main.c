9183a54f37a3f5d774ac79676afdaa146ef6a80a
Sandor Mate <sandor.mate@gmx.com>
2018-03-25 17:34:03 +0200

    2nd commit

--- Demo/main.c
+++ Demo/main.c
@@ -5,7 +5,8 @@
 int main()
 {
     char string1[] = "Hey Buddy!";
+    printf("The original string: %s\n", string1);
     ToUppercase(string1);
-    printf("%s\n", string1);
+    printf("After calling ToUppercase(string1): %s\n", string1);
     return 0;
 }
