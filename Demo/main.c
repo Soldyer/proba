7b0ff6318d594271d9b0a8faaa1f71302b298de1
Sandor Mate <sandor.mate@gmx.com>
2018-04-28 10:58:05 +0200

    Indentation

--- Demo/main.c
+++ Demo/main.c
@@ -5,8 +5,10 @@
 int main()
 {
     char string1[] = "Hey Buddy!";
+
     printf("The original string: %s\n", string1);
     ToUppercase(string1);
     printf("After calling ToUppercase(string1): %s\n", string1);
+
     return 0;
 }
